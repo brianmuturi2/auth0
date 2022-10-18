bdaf4de8e06d4f7bfa4612ed55d667afada5ca3e
brianmuturi2 <brianmuturi2@gmail.com>
2022-10-18 17:21:13 +0300

    feat(auth): added interceptor

--- src/app/pages/external-api/external-api.component.ts
+++ src/app/pages/external-api/external-api.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment as env} from '../../../environments/environment';
+
+interface MessageRes {
+  message: string;
+}
 
 @Component({
   selector: 'app-external-api',
@@ -7,7 +13,20 @@ import { Component, OnInit } from '@angular/core';
 export class ExternalApiComponent implements OnInit {
   message: string = null;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {}
+
+  callApi(): void {
+    this.http.get(`${env.dev.apiUrl}/api/messages/public-message`).subscribe((res: MessageRes) => {
+      this.message = res.message;
+    });
+  }
+
+  callSecureApi(): void {
+    this.http.get(`${env.dev.apiUrl}/api/messages/protected-message`).subscribe((res: MessageRes) => {
+      this.message = res.message;
+    });
+  }
+
 }
